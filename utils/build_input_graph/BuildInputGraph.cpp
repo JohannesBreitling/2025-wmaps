32d40ceefae3e02972de6053012607abd465d372
Johannes Breitling <j.breitling@suited-technologies.com>
2025-01-15 21:01:31 +0100

    Updated the launch.json to include pugi the right way and started with the parsing of the input

--- utils/build_input_graph/BuildInputGraph.cpp
+++ utils/build_input_graph/BuildInputGraph.cpp
@@ -3,7 +3,11 @@
 #include "../pugixml/pugixml.hpp"
 
 int main(int numArgs, char** args) {
-    std::cout << "Hello, from the input graph building!\n";
+    std::cout << "- - - - - - - - - - - - - - - - -\n";
+    std::cout << "-           W M A P S           -\n";
+    std::cout << "- - - - - - - - - - - - - - - - -\n";
+    std::cout << "-       BUILD INPUT GRAPH       -\n";
+    std::cout << "- - - - - - - - - - - - - - - - -\n";
 
     // Get the file path of the input graph osm file
     if (numArgs < 2) {
@@ -11,9 +15,29 @@ int main(int numArgs, char** args) {
         return 1;
     }
 
-    std::string inputPath = args[1];
+    const char* inputPath = args[1];
+    
+    pugi::xml_document doc;
+    pugi::xml_parse_result result = doc.load_file(inputPath);
 
-    std::cout << "Das ist der Path : " << inputPath << std::endl;
+    if (!result) {
+        std::cerr << "Error when parsing the input file: " << result.description() << "\n";
+        std::cerr << "Provided file path: " << inputPath << "\n";
+        return 1;
+    }
+
+    pugi::xml_node root = doc.child("osm");
+
+    for (pugi::xml_node node : root.children("node")) {
+        int id = node.attribute("id").as_int();
+        std::cout << "Parse node with id : " << id << "\n";
+    }
+    
+
+
+    
+
+    std::cout << "Successfully parsed the input file!\n";
 
     return 0;
 }
